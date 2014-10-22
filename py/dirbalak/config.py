5491e9d6b31e77d713aa771c020918a51b1410a3
Shlomi Matichin <shlomi@stratoscale.com>
2014-10-22 14:12:05 +0000

    Default rootfs now based on lvm-enabled rootfs

--- py/dirbalak/config.py
+++ py/dirbalak/config.py
@@ -10,9 +10,9 @@ REPO_MIRRORS_LOCKFILE = os.path.join(REPO_MIRRORS_BASEDIR, "lock")
 BUILD_CHROOT = os.path.join(DIRBALAK_DIR, "chroot")
 BUILD_DIRECTORY = os.path.join(BUILD_CHROOT, "home", "dirbalak")
 
-OFFICIAL_BUILD_ROOTFS = "rootfs-build__rootfs__e748e16567567a16d6112876c4b95ab88b64d76a__bootstrap"
+OFFICIAL_BUILD_ROOTFS = "rootfs-build__rootfs__4f3e35e69f5014eaeda422c21def14d36ca1972d__bootstrap"
 NO_DIRBALAK_MANIFEST_BUILD_ROOTFS = \
-    "rootfs-build__rootfs__e748e16567567a16d6112876c4b95ab88b64d76a__bootstrap"
+    "rootfs-build__rootfs__4f3e35e69f5014eaeda422c21def14d36ca1972d__bootstrap"
 
 LOGBEAM_ROOT_DIR = "dirbalak"
 CLEANBUILD_LOG_FILENAME = "dirbalak.cleanbuild.log"
