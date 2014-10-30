ec45c173c1c105fd17e87491f3e5d62c5403e874
Shlomi Matichin <shlomi@stratoscale.com>
2014-10-30 19:30:38 +0000

    New rootfs with solvent supporting mount ops

--- py/dirbalak/config.py
+++ py/dirbalak/config.py
@@ -10,9 +10,9 @@ REPO_MIRRORS_LOCKFILE = os.path.join(REPO_MIRRORS_BASEDIR, "lock")
 BUILD_CHROOT = os.path.join(DIRBALAK_DIR, "chroot")
 BUILD_DIRECTORY = os.path.join(BUILD_CHROOT, "home", "dirbalak")
 
-OFFICIAL_BUILD_ROOTFS = "rootfs-build__rootfs__f0e90d5f17ed12b4153eb4d832e07a333a016d66__bootstrap"
+OFFICIAL_BUILD_ROOTFS = "rootfs-build__rootfs__655d019312a83701dab42bfb9cfc94602a895460__bootstrap"
 NO_DIRBALAK_MANIFEST_BUILD_ROOTFS = \
-    "rootfs-build__rootfs__f0e90d5f17ed12b4153eb4d832e07a333a016d66__bootstrap"
+    "rootfs-build__rootfs__655d019312a83701dab42bfb9cfc94602a895460__bootstrap"
 
 LOGBEAM_ROOT_DIR = "dirbalak"
 CLEANBUILD_LOG_FILENAME = "dirbalak.cleanbuild.log"
